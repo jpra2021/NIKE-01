49e6b26b95656fbab9ec28744eaf31dfa38626dc
EH <chi0727139@gmail.com>
2022-08-25 15:23:13 +0900

    [front-fix] fix typo redirect.js

--- front/src/components/Redirect.js
+++ front/src/components/Redirect.js
@@ -1,6 +1,4 @@
-import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Spinner } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
