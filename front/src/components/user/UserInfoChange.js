59a61e117bb210f9b32ce884cff6d3f58634ef7d
EH <chi0727139@gmail.com>
2022-08-30 19:24:23 +0900

    [front-update] update UserInfoChange for user convenience

--- front/src/components/user/UserInfoChange.js
+++ front/src/components/user/UserInfoChange.js
@@ -1,26 +1,28 @@
 import React, { useState, useContext, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { Container, Col, Row, Form, Button } from "react-bootstrap";
-import { NoticeContext, UserStateContext } from "../../App";
+import { NoticeContext } from "../../App";
 import * as API from "../../api";
 
 function UserInfoChange() {
   const navigate = useNavigate();
   const { state } = useLocation();
-  
-  const [ formData, setFormData ] = useState({
+  const { setNotices } = useContext(NoticeContext);
+
+  const [formData, setFormData] = useState({
     email: "",
     password: "",
     confirmPassword: "",
   });
 
-  const { setNotices } = useContext(NoticeContext);
-  const userState = useContext(UserStateContext);
+  // 이메일, 비밀번호 중 무엇을 변경할 것인지 유저가 선택
+  const [chooseEmail, setChooseEmail] = useState(false);
+  const [choosePassword, setChoosePassword] = useState(false);
 
   useEffect(() => {
-    if(!state?.isAuth) {
+    if (!state?.isAuth) {
       navigate("/");
-      return; 
+      return;
     }
   }, []);
 
@@ -37,103 +39,157 @@ function UserInfoChange() {
   const isPasswordSame = formData.password === formData.confirmPassword;
   const isFormValid = isEmailValid && isPasswordValid && isPasswordSame;
 
+  const formValidation = () => {
+    if (chooseEmail && choosePassword) {
+      return !isFormValid;
+    } else if (chooseEmail && !choosePassword) {
+      return !isEmailValid;
+    } else if (!chooseEmail && choosePassword) {
+      return !isPasswordValid || !isPasswordSame;
+    } else {
+      return true;
+    }
+  };
+
   const handleFormChange = (e) => {
     const { name, value } = e.target;
 
     setFormData((current) => {
       return { ...current, [name]: value };
-    })
-  }
-
-  const editUserData = async () => {
-    const res = await API.put("/users/edit", {email: formData.email, password: formData.password});
-  }
+    });
+  };
 
   const handleClick = (e) => {
     e.preventDefault();
 
-    // 이메일, 비밀번호 변경하기
     try {
-      setNotices({ type: "success", payload: {title: "변경 성공!", message: "이메일과 비밀번호가 변경되었습니다."}});
-      const user = userState.user;
-      API.put("users/edit", {email: formData.email, password: formData.password})
-        .then(res => console.log(res));
+      //이메일 변경
+      if (chooseEmail) {
+        API.put("users/edit", {
+          email: formData.email,
+        });
+      }
+
+      //비밀번호 변경
+      if (choosePassword) {
+        API.put("users/edit", {
+          password: formData.password,
+        });
+      }
+
+      setNotices({
+        type: "success",
+        payload: {
+          title: "변경 성공!",
+          message: "변경되었습니다.",
+        },
+      });
 
       navigate("/");
       return;
     } catch (err) {
-      console.log("이메일, 비밀번호 변경에 실패하였습니다.\n", err);
+      console.log("변경에 실패하였습니다.\n", err);
     }
   };
 
   return (
     <Container>
-      <Row className="justify-content-md-center mt-5">
+      <Row className="justify-content-center mt-5">
         <Form.Text
-          className="justify-content-md-center mb-5 text-primary"
+          className="justify-content-center mb-4 text-primary"
           style={{ display: "flex", fontSize: 17 }}
         >
-          기존 이메일과 비밀번호가 아래와 같이 변경됩니다.
+          변경하고 싶은 것을 모두 체크해주세요.
         </Form.Text>
+
+        <Form
+          className="mb-4"
+          style={{ display: "flex", justifyContent: "center" }}
+        >
+          <Form.Check
+            inline
+            label="이메일"
+            name="selectEmail"
+            type="checkbox"
+            id="checkbox-1"
+            onChange={(e) => setChooseEmail((prev) => !prev)}
+          />
+          <Form.Check
+            inline
+            label="비밀번호"
+            name="selectPassword"
+            type="checkbox"
+            id="checkbox-2"
+            onChange={(e) => setChoosePassword((prev) => !prev)}
+          />
+        </Form>
 
         <Col xs={5}>
           <Form>
-            <Form.Group controlId="changeEmail">
-              <Form.Label>이메일 주소</Form.Label>
-              <Form.Control
-                type="email"
-                autoComplete="on"
-                placeholder="email"
-                name="email"
-                value={formData.email}
-                onChange={handleFormChange}
-              />
-              {!isEmailValid && (
-                <Form.Text className="text-success">
-                  이메일 형식이 올바르지 않습니다.
-                </Form.Text>
-              )}
-            </Form.Group>
+            {/* 이메일 변경 선택 */}
+            {chooseEmail && (
+              <Form.Group controlId="changeEmail" className="mb-3">
+                <Form.Label>이메일 주소</Form.Label>
+                <Form.Control
+                  type="email"
+                  autoComplete="on"
+                  placeholder="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleFormChange}
+                />
+                {!isEmailValid && (
+                  <Form.Text className="text-success">
+                    이메일 형식이 올바르지 않습니다.
+                  </Form.Text>
+                )}
+              </Form.Group>
+            )}
 
-            <Form.Group controlId="changePassword" className="mt-3">
-              <Form.Label>비밀번호</Form.Label>
-              <Form.Control
-                type="password"
-                autoComplete="off"
-                placeholder="password"
-                name="password"
-                value={formData.password}
-                onChange={handleFormChange}
-              />
-              {!isPasswordValid && (
-                <Form.Text className="text-success">
-                  비밀번호는 4글자 이상으로 설정해 주세요.
-                </Form.Text>
-              )}
-            </Form.Group>
+            {/* 비밀번호 변경 선택 */}
+            {choosePassword && (
+              <>
+                <Form.Group controlId="changePassword">
+                  <Form.Label>비밀번호</Form.Label>
+                  <Form.Control
+                    type="password"
+                    autoComplete="off"
+                    placeholder="password"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleFormChange}
+                  />
+                  {!isPasswordValid && (
+                    <Form.Text className="text-success">
+                      비밀번호는 4글자 이상으로 설정해 주세요.
+                    </Form.Text>
+                  )}
+                </Form.Group>
 
-            <Form.Group controlId="confirmChangePassword" className="mt-3">
-              <Form.Label>비밀번호 재확인</Form.Label>
-              <Form.Control
-                type="password"
-                autoComplete="off"
-                placeholder="confirm password"
-                name="confirmPassword"
-                value={formData.confirmPassword}
-                onChange={handleFormChange}
-              />
-              {!isPasswordSame && (
-                <Form.Text className="text-success">
-                  비밀번호가 일치하지 않습니다.
-                </Form.Text>
-              )}
-            </Form.Group>
+                <Form.Group controlId="confirmChangePassword" className="mt-3">
+                  <Form.Label>비밀번호 재확인</Form.Label>
+                  <Form.Control
+                    type="password"
+                    autoComplete="off"
+                    placeholder="confirm password"
+                    name="confirmPassword"
+                    value={formData.confirmPassword}
+                    onChange={handleFormChange}
+                  />
+                  {!isPasswordSame && (
+                    <Form.Text className="text-success">
+                      비밀번호가 일치하지 않습니다.
+                    </Form.Text>
+                  )}
+                </Form.Group>
+              </>
+            )}
 
             <Form.Group as={Row} className="mt-3 text-center">
               <Col sm={{ span: 20 }}>
                 <Button
                   variant="primary"
-                  disabled={!isFormValid}
+                  disabled={formValidation()}
                   onClick={handleClick}
                 >
                   변경하기
