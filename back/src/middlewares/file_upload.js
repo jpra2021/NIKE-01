fa3506c98a5292dbb42e2873f1c45451a49c4774
jpra2021 <jthvmcm2021@cuk.edu>
2022-08-31 22:31:53 +0900

    [ET1-124/back/upgrade] multer singleUpload

--- back/src/middlewares/file_upload.js
+++ back/src/middlewares/file_upload.js
@@ -4,16 +4,13 @@ import path from "path";
 
 /* checking existence of folder */
 try {
-  fs.readdirSync("LocalUpload");
-  console.log("Localupload 폴더가 있습니다.");
+  fs.readdirSync("LocalFile");
+  console.log("LocalFile 폴더가 있습니다.");
 } catch (err) {
-  console.error("LocalUpload 폴더가 없습니다. 폴더를 생성합니다.");
-  fs.mkdirSync("LocalUpload");
+  console.error("LocalFile 폴더가 없습니다. 폴더를 생성합니다.");
+  fs.mkdirSync("LocalFile");
 }
-
-//const file_upload = multer({ storage: storage })
-
-/* ---storage setting--- */
+/*---multer to file upload---*/
 
 const file_upload = multer({
   storage: multer.diskStorage({
@@ -24,59 +21,8 @@ const file_upload = multer({
       const user_id = req.currentUserId;
       const ext = path.extname(file.originalname);
       const newFilename = `nike_${user_id}_${Date.now()}${ext}`;
-      console.log("multe결과값", newFilename);
       cb(null, newFilename);
     },
   }),
 });
 export { file_upload };
-
-// function file_upload_single(req, res, next) {
-//   try {
-//     const singleUpload = multer({ storage: storage }).single(req.file);
-//     const upload = util.promisify(singleUpload);d
-
-//     next();
-//   } catch (error) {
-//     res.status(400).send("파일 업로드 실패(미들웨어 오류");
-//     return;
-//   }
-// }
-
-/* try catch */
-// async function upload_single(req, res, next) {
-//   try {
-//     const uploadedFile = multer({ storage: storage }).single(req.body.PLZFile);
-//     console.log("middleware", uploadedFile);
-//     next();
-//   } catch (error) {
-//     res.status(400).send("multer middleware에서 문제가 생김");
-//     return;
-//   }
-// }
-
-// export { upload_single };
-
-/* shorted */
-// const file_upload = multer({ storage: storage });
-// export { file_upload };
-
-// /* ---storage setting--- */
-// const storage = multer.diskStorage({
-//     destination(req, file, cb) {
-//       //console.log("스토리지, destination-req", req, "\n-----------------");
-//       //console.log("스토리지, destination-file", file, "\n-----------------");
-//       cb(null, "/LocalUpload");
-//     },
-//     filename(req, file, cb) {
-//       //to get file extension
-//       const originalName = file.originalname;
-//       //console.log("스토리지, filename, original", originalName);
-//       ////const ext = path.extname(originalName);
-//       //console.log("스토리지, filename, ext", ext);
-//       //const newFilename = `${originalName}__${Date.now()}`;
-//       //console.log("스토리지, filename, newFilename", newFilename);
-//       const newFilename = file.filename + "-" + Date.now();
-//       cb(null, newFilename);
-//     },
-//   });
--- back/src/routers/fileRouter.js
+++ back/src/routers/fileRouter.js
@@ -1,41 +1,30 @@
 import { Router } from "express";
 import { file_upload } from "../middlewares/file_upload";
-import fs from "fs";
 import { login_required } from "../middlewares/login_required";
 import { fileService } from "../services/fileService";
 const fileRouter = Router();
 const uploadSingle = file_upload.single("file");
-/* CREATE */
+/* CREATE Single File*/
 fileRouter.post(
   "/users/upload",
   login_required,
   uploadSingle,
-  //스토리컴에 저장이 되는거고
   async (req, res, next) => {
-    //저장된걸 mongk db 정보처리
     try {
-      //console.log("File Uploaded 라우터에서:::", req.file);
       const user_id = req.currentUserId;
       const fileName = req.file.filename;
-      //console.log("라우터 filename", fileName);
-      const decoded = decodeURIComponent(fileName);
-      //console.log("라우터 타입?", decoded);
-      //console.log("라우터 decoded", decoded);
       const fileExt = req.file.fieldname;
       const fileSrc = req.file.path;
       const fileSize = req.file.size;
-
       const newFileValue = { user_id, fileName, fileExt, fileSrc, fileSize };
-      //console.log("무엇이 전달되는가?(rout to ser)", newFileValue);
 
       const newFile = await fileService.createFileSrc(newFileValue);
 
       if (newFile.errorMessage) {
-        throw new Error(newFile.errorMessage);
+        res.json({ ok: false });
+        return;
       }
-      //console.log("Saved To database");
-      //console.log("uploadFile:", newFile);
-      res.send("ok");
+      res.json({ ok: true });
     } catch (error) {
       next(error);
     }
